0c6ab6e7b66392064e2ec8a1ab746c4032aed6f8
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-10-25 19:33:20 +0700

    mulai fitur api

--- assets/js/localJs/index.js
+++ assets/js/localJs/index.js
@@ -1,10 +1,10 @@
-
-
 // fitur icon bar
 
-let icon_bar = document.querySelector('.icon-bar')
-let nav_item =  document.querySelector('.nav-item')
-icon_bar.addEventListener('click',function(){
-    nav_item.classList.toggle('nav-item-active')
-    console.log('ok')
-})
+let icon_bar = document.querySelector('.icon-bar');
+let nav_item = document.querySelector('.nav-item');
+icon_bar.addEventListener('click', function () {
+  nav_item.classList.toggle('nav-item-active');
+  console.log('ok');
+});
+
+// fitur api
