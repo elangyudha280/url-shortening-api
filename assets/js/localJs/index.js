9606904c4d00d97a1562004f3affb89ecd3a76c8
elangyudha280 <elangyudharakasiwi@gmail.com>
2022-10-27 18:44:29 +0700

    fix bugs

--- assets/js/localJs/index.js
+++ assets/js/localJs/index.js
@@ -2,13 +2,22 @@
 
 let nav_item = document.querySelector('.nav-item');
 
+let icon_bar = document.querySelector('.icon-bar');
 
-document.addEventListener('click',(e)=>{
-  if(e.target.classList.contains('icon-bar')){
-    nav_item.classList.add('nav-item-active');
-    return 
-  }
-  nav_item.classList.remove('nav-item-active');
+icon_bar.addEventListener('click',()=>{
+  nav_item.classList.toggle('nav-item-active')
+  icon_bar.classList.toggle('icon-bar-active')
 })
-
 // fitur api
+fetch('https://api.shrtco.de/v2/').then(Response => {
+  if(!Response.ok){
+    return Response.json()
+  }
+  return Response.json()
+})
+.then(e => {
+  console.log(e)
+})
+.catch(err => {
+  console.log(err)
+})
